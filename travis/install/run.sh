e3b522697c07970961eda16fe48056ac8035949a
Junjie Bai <bai@in.tum.de>
2017-11-28 01:40:14 -0500

    Add timing

--- travis/install/run.sh
+++ travis/install/run.sh
@@ -5,26 +5,26 @@ source "$top_dir/setup.sh"
 
 # Install caffe
 ccache -z
-pip install -b "$BUILD_DIR/caffe2" "file://$REPOS_DIR/caffe2#egg=caffe2"
+time pip install -b "$BUILD_DIR/caffe2" "file://$REPOS_DIR/caffe2#egg=caffe2"
 ccache -s
 
 # Install onnx
 ccache -z
-pip install -b "$BUILD_DIR/onnx" -e "file://$REPOS_DIR/onnx#egg=onnx"
+time pip install -b "$BUILD_DIR/onnx" -e "file://$REPOS_DIR/onnx#egg=onnx"
 ccache -s
 
 # Install onnx-caffe2
 ccache -z
-pip install -b "$BUILD_DIR/onnx-caffe2" -e "file://$REPOS_DIR/onnx-caffe2#egg=onnx-caffe2"
+time pip install -b "$BUILD_DIR/onnx-caffe2" -e "file://$REPOS_DIR/onnx-caffe2#egg=onnx-caffe2"
 ccache -s
 
 # Install pytorch
 pip install pyaml
 ccache -z
-pip install -b "$BUILD_DIR/pytorch" "file://$REPOS_DIR/pytorch#egg=torch"
+time pip install -b "$BUILD_DIR/pytorch" "file://$REPOS_DIR/pytorch#egg=torch"
 ccache -s
 
 # Install onnx-pytorch
 ccache -z
-pip install -b "$BUILD_DIR/onnx-pytorch" "file://$REPOS_DIR/onnx-pytorch#egg=onnx-pytorch"
+time pip install -b "$BUILD_DIR/onnx-pytorch" "file://$REPOS_DIR/onnx-pytorch#egg=onnx-pytorch"
 ccache -s
