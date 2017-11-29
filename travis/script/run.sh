ee808984fce4c8a6bf5e021f1524b6500f9a0ef6
Junjie Bai <bai@in.tum.de>
2017-11-29 06:00:20 -0500

    Do not run tests in onnx and onnx-caffe2 repo

--- travis/script/run.sh
+++ travis/script/run.sh
@@ -3,16 +3,6 @@
 top_dir=$(dirname $(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}"))))
 source "$top_dir/setup.sh"
 
-# Run tests in onnx repo
-pip install pytest-cov nbval
-cd "$REPOS_DIR/onnx"
-pytest
-
-# Run tests in onnx-caffe2 repo
-pip install psutil tabulate
-cd "$REPOS_DIR/onnx-caffe2"
-pytest
-
 # Run tests in onnx-pytorch repo
 cd "$REPOS_DIR/onnx-pytorch"
 make test
