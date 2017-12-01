3bc5e97d5bf352a8d03686d649c1643d56ee6af8
bddppq <bai@in.tum.de>
2017-11-30 23:50:41 -0800

    Only clear and show ccache stats in CI

--- install.sh
+++ install.sh
@@ -12,28 +12,32 @@ REPOS_DIR="$top_dir/repos"
 BUILD_DIR="$top_dir/build"
 mkdir -p "$BUILD_DIR"
 
+_pip_install() {
+    if [[ -n "$CI" ]]; then
+        ccache -z
+    fi
+    if [[ -n "$CI" ]]; then
+        time pip install "$@"
+    else
+        pip install "$@"
+    fi
+    if [[ -n "$CI" ]]; then
+        ccache -s
+    fi
+}
+
 # Install caffe2
-ccache -z
-time pip install -b "$BUILD_DIR/caffe2" "file://$REPOS_DIR/caffe2#egg=caffe2"
-ccache -s
+_pip_install -b "$BUILD_DIR/caffe2" "file://$REPOS_DIR/caffe2#egg=caffe2"
 
 # Install onnx
-ccache -z
-time pip install -b "$BUILD_DIR/onnx" "file://$REPOS_DIR/onnx#egg=onnx"
-ccache -s
+_pip_install -b "$BUILD_DIR/onnx" "file://$REPOS_DIR/onnx#egg=onnx"
 
 # Install onnx-caffe2
-ccache -z
-time pip install -b "$BUILD_DIR/onnx-caffe2" "file://$REPOS_DIR/onnx-caffe2#egg=onnx-caffe2"
-ccache -s
+_pip_install -b "$BUILD_DIR/onnx-caffe2" "file://$REPOS_DIR/onnx-caffe2#egg=onnx-caffe2"
 
 # Install pytorch
 pip install -r "$REPOS_DIR/pytorch/requirements.txt"
-ccache -z
-time pip install -b "$BUILD_DIR/pytorch" "file://$REPOS_DIR/pytorch#egg=torch"
-ccache -s
+_pip_install -b "$BUILD_DIR/pytorch" "file://$REPOS_DIR/pytorch#egg=torch"
 
 # Install onnx-pytorch
-ccache -z
-time pip install -b "$BUILD_DIR/onnx-pytorch" "file://$REPOS_DIR/onnx-pytorch#egg=onnx-pytorch"
-ccache -s
+_pip_install -b "$BUILD_DIR/onnx-pytorch" "file://$REPOS_DIR/onnx-pytorch#egg=onnx-pytorch"
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,7 @@
 set -ex
 
+export CI=true
+
 export TOP_DIR=$(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}")))
 
 export OS="$(uname)"
