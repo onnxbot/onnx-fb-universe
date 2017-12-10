d0bd52ee338077e2cc6474e1acc1d3e63ebc12fb
bddppq <bai@in.tum.de>
2017-12-09 16:07:12 -0800

    Fix jenkins builds ccache setup (#12)

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -10,7 +10,16 @@ export OS="$(uname)"
 if [[ "$OS" == "Darwin" ]]; then
     export PATH="/usr/local/opt/ccache/libexec:$PATH"
 else
-    export PATH="/usr/lib/ccache:$PATH"
+    if [[ -d "/usr/lib/ccache" ]]; then
+        export PATH="/usr/lib/ccache:$PATH"
+    elif hash ccache > /dev/null; then
+        mkdir -p "$TOP_DIR/ccache"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/cc"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/c++"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/gcc"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/g++"
+        export PATH="$TOP_DIR/ccache:$PATH"
+    fi
 fi
 
 # setup virtualenv
