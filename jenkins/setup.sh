149c8ff62f41342dc71e189f76e7f7ddddc6d9e8
bddppq <bai@in.tum.de>
2017-12-15 23:30:59 -0800

    Revert "Disable ccache temporarily"
    This reverts commit 8a21ae07f9d4a568a1b4d3f4a558ac2ef009dfc1.

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -6,22 +6,22 @@ export TOP_DIR=$(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}")))
 
 export OS="$(uname)"
 
-# # setup ccache
-# if [[ "$OS" == "Darwin" ]]; then
-#     export PATH="/usr/local/opt/ccache/libexec:$PATH"
-# else
-#     if [[ -d "/usr/lib/ccache" ]]; then
-#         export PATH="/usr/lib/ccache:$PATH"
-#     elif hash ccache > /dev/null; then
-#         mkdir -p "$TOP_DIR/ccache"
-#         ln -sf "$(which ccache)" "$TOP_DIR/ccache/cc"
-#         ln -sf "$(which ccache)" "$TOP_DIR/ccache/c++"
-#         ln -sf "$(which ccache)" "$TOP_DIR/ccache/gcc"
-#         ln -sf "$(which ccache)" "$TOP_DIR/ccache/g++"
-#         ln -sf "$(which ccache)" "$TOP_DIR/ccache/x86_64-linux-gnu-gcc"
-#         export PATH="$TOP_DIR/ccache:$PATH"
-#     fi
-# fi
+# setup ccache
+if [[ "$OS" == "Darwin" ]]; then
+    export PATH="/usr/local/opt/ccache/libexec:$PATH"
+else
+    if [[ -d "/usr/lib/ccache" ]]; then
+        export PATH="/usr/lib/ccache:$PATH"
+    elif hash ccache > /dev/null; then
+        mkdir -p "$TOP_DIR/ccache"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/cc"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/c++"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/gcc"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/g++"
+        ln -sf "$(which ccache)" "$TOP_DIR/ccache/x86_64-linux-gnu-gcc"
+        export PATH="$TOP_DIR/ccache:$PATH"
+    fi
+fi
 
 # setup virtualenv
 virtualenv "$TOP_DIR/venv"
