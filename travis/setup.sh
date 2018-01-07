6ff2ec7af9c54540a8ff17fc0cc19e370797c340
bddppq <bai@in.tum.de>
2018-01-07 12:40:44 -0800

    Update exisiting python packages in travis python

--- travis/setup.sh
+++ travis/setup.sh
@@ -5,3 +5,6 @@ top_dir=$(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}")))
 # setup ccache
 export PATH="/usr/lib/ccache:$PATH"
 ccache --max-size 1G
+
+# Update all existing python packages
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
