12f29be67affcf9f57ce057cc4b29e166b788aec
Lu Fang <lufang@fb.com>
2018-03-15 11:45:54 -0700

    Sync the script back to GH

--- scripts/onnx-dev-setup.sh
+++ scripts/onnx-dev-setup.sh
@@ -9,12 +9,36 @@
 
 # TODO: support python 3.
 
-set -ex
+# Set script configuration
+set -e
 shopt -s expand_aliases
+
+# Proxy setup
+alias with_proxy="HTTPS_PROXY=http://fwdproxy.any:8080 HTTP_PROXY=http://fwdproxy.any:8080 FTP_PROXY=http://fwdproxy.any:8080 https_proxy=http://fwdproxy.any:8080 http_proxy=http://fwdproxy.any:8080 ftp_proxy=http://fwdproxy.any:8080 http_no_proxy='*.facebook.com|*.tfbnw.net|*.fb.com'"
+
+# Set the variables
 CYAN='\033[0;36m'
 NC='\033[0m'
+onnx_root="$HOME/onnx-dev"   # I think hardcoding the onnx root dir is fine, just like fbsource
+venv="$onnx_root/onnxvenv"
+onnx_init_file="$onnx_root/.onnx_env_init"
+ccache_root="$onnx_root/ccache"
+ccache_script="$onnx_root/ccache_install.sh"
 
-# Checking to see if CuDNN is present
+# Check whether you put nvcc in PATH
+set +e
+nvcc_path=$(which nvcc)
+if [[ -z $nvcc_path ]]; then
+  nvcc_path="/usr/local/cuda/bin/nvcc"
+fi
+set -e
+if [ ! -f "$nvcc_path" ] && ! $force; then
+  echo "nvcc is not detected in $PATH"
+  exit 1
+fi
+echo "nvcc is detected at $nvcc_path"
+
+# Checking to see if CuDNN is present, and install it if not exists
 if [ -f /usr/local/cuda/include/cudnn.h ]; then
   echo "CuDNN header already exists!!"
 else
@@ -22,98 +46,99 @@ else
   sudo cp -R /home/engshare/third-party2/cudnn/6.0.21/src/cuda/lib64/* /usr/local/cuda/lib64/
 fi
 
-# make sure we find CUDA and that nvcc is runnable
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-nvcc --version
-
-# Install Caffe2 requirements from the internal yum repo
+# TODO set the specific version for each package
+# Install the dependencies for Caffe2
 sudo yum install python-virtualenv freetype-devel libpng-devel glog gflags protobuf protobuf-devel protobuf-compiler -y
+rpm -q protobuf  # check the version and if necessary update the value below
+protoc --version  # check protoc
 
-# Installing cmake
+# Upgrade Cmake to the right version (>3.0)
 sudo yum remove cmake3 -y
 sudo yum install cmake -y
+
+# Install the dependencies for CCache
 sudo yum install autoconf asciidoc -y
 
-# Proxy setup
-alias with_proxy="HTTPS_PROXY=http://fwdproxy.any:8080 HTTP_PROXY=http://fwdproxy.any:8080 FTP_PROXY=http://fwdproxy.any:8080 https_proxy=http://fwdproxy.any:8080 http_proxy=http://fwdproxy.any:8080 ftp_proxy=http://fwdproxy.any:8080 http_no_proxy='*.facebook.com|*.tfbnw.net|*.fb.com'"
+# Create the root folder
+if [ -e "$onnx_root" ]; then
+  timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
+  mv --backup=t "$onnx_root" "$onnx_root"."$timestamp"
+fi
+mkdir -p "$onnx_root"
 
 # Set the name of virtualenv instance
-onnxroot="$HOME/onnx-dev"
-
-if [ -d "$onnxroot" ]; then
-  timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
-  mv --backup=t "$onnxroot" "$onnxroot"."$timestamp"
-fi
-mkdir -p "$onnxroot"
-
-venv="$onnxroot/onnxvenv"
-
-# Create a virtualenv, activate it, upgrade pip
 with_proxy virtualenv "$venv"
-# shellcheck disable=SC1090
-source "$venv/bin/activate"
-with_proxy pip install pip setuptools -U
-
-# Install other Caffe2 requirements
-rpm -q protobuf # check the version and if necessary update the value below
-# Todo - Add Grep to find protobuf version
-with_proxy pip install future numpy "protobuf>3.2" ninja pytest-runner
-
-# Installing CCache
-ccache_root="$onnxroot/ccache"
-rm -rf "$ccache_root"
-cd "$onnxroot"
-with_proxy git clone https://github.com/colesbury/ccache.git -b ccbin
-cd "$ccache_root"
-./autogen.sh
-./configure
-make install prefix="$ccache_root"
-
-mkdir -p "$ccache_root/lib"
-mkdir -p "$ccache_root/cuda"
-ln -sf "$ccache_root/bin/ccache" "$ccache_root/lib/cc"
-ln -sf "$ccache_root/bin/ccache" "$ccache_root/lib/c++"
-ln -sf "$ccache_root/bin/ccache" "$ccache_root/lib/gcc"
-ln -sf "$ccache_root/bin/ccache" "$ccache_root/lib/g++"
-ln -sf "$ccache_root/bin/ccache" "$ccache_root/cuda/nvcc"
-"$ccache_root"/bin/ccache -M 25Gi
-
-export PATH="$ccache_root/lib:/usr/local/cuda/bin:$PATH"
-export CUDA_NVCC_EXECUTABLE="$ccache_root/cuda/nvcc"
-
-# Make sure the nvcc wrapped in CCache is runnable
-"$ccache_root/cuda/nvcc" --version
-
-# Checking to see if the environment variable script is present
-onnx_init_file="$onnxroot/.onnx_env_init"
-if [ -f "$onnx_init_file" ]; then
-    echo "Environment variable script already exists!! Moving the old version to a backup in order to generate a new one"
-    mv --backup=t "$onnx_init_file" "$onnx_init_file".old
-fi
 
 # Creating a script that can be sourced in the future for the environmental variable
 touch "$onnx_init_file"
 {
-  echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH";
-  echo "export PATH=~/ccache/lib:/usr/local/cuda/bin:$PATH";
-  echo "export CUDA_NVCC_EXECUTABLE=$ccache_root/cuda/nvcc";
-  echo "source ${venv}/bin/activate";
+  echo -e "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\x24LD_LIBRARY_PATH";
+  echo -e "export PATH=$ccache_root/lib:/usr/local/cuda/bin:\x24PATH";
+  echo "source $venv/bin/activate";
+  echo 'alias with_proxy="HTTPS_PROXY=http://fwdproxy.any:8080 HTTP_PROXY=http://fwdproxy.any:8080 FTP_PROXY=http://fwdproxy.any:8080 https_proxy=http://fwdproxy.any:8080 http_proxy=http://fwdproxy.any:8080 ftp_proxy=http://fwdproxy.any:8080 http_no_proxy='"'"'*.facebook.com|*.tfbnw.net|*.fb.com'"'"'"'
 } >> "$onnx_init_file"
 chmod u+x "$onnx_init_file"
 
+# Installing CCache
+cd "$onnx_root"
+with_proxy wget https://raw.githubusercontent.com/onnxbot/onnx-fb-universe/master/scripts/ccache_setup.sh -O "$ccache_script"
+chmod u+x "$ccache_script"
+"$ccache_script" --path "$ccache_root"
+
+# Test nvcc with CCache
+own_ccache=true
+if [ -f "$CUDA_NVCC_EXECUTABLE" ] && [[ "$ccache_root/cuda/nvcc" == "$CUDA_NVCC_EXECUTABLE" ]]; then
+  "$CUDA_NVCC_EXECUTABLE" --version
+  if [ $? == 0 ]; then
+    own_ccache=false
+  fi
+fi
+if $own_ccache; then
+  echo "export CUDA_NVCC_EXECUTABLE=$ccache_root/cuda/nvcc" >> "$onnx_init_file"
+fi
+
+# Loading env vars
+source "$onnx_init_file"
+
+"$CUDA_NVCC_EXECUTABLE" --version
+
+# Create a virtualenv, activate it, upgrade pip
+if [ -f "$HOME/.pip/pip.conf"]; then
+  echo "Warning: $HOME/.pip/pip.conf is detected, pip install may fail!"
+fi
+# shellcheck disable=SC1090
+with_proxy python -m pip install -U pip setuptools
+with_proxy python -m pip install future numpy "protobuf>3.2" pytest-runner pyyaml typing ipython
+
 # Cloning repos
-cd "$onnxroot"
+cd "$onnx_root"
 with_proxy git clone https://github.com/onnxbot/onnx-fb-universe --recursive
 
-# Installing packages in develop mode with install script
-cd onnx-fb-universe
-with_proxy ./install-develop.sh
+# Build ONNX
+cd "$onnx_root/onnx-fb-universe/repos/onnx"
+with_proxy python setup.py develop
 
-# Sanity Checks
+# Build PyTorch
+cd "$onnx_root/onnx-fb-universe/repos/pytorch"
+with_proxy pip install -r "requirements.txt"
+with_proxy python setup.py build develop
+
+# Build Caffe2
+set +e
+ninja_path=$(which ninja)
+set -e
+if [[ ! -z $ninja_path ]]; then
+  echo "Warning: ninja is installed at $ninja_path, which may cause Caffe2 building issue!!!"
+fi
+cd "$onnx_root/onnx-fb-universe/repos/caffe2"
+with_proxy python setup.py develop
+
+# Sanity checks and useful info
 python -c 'from caffe2.python import build; from pprint import pprint; pprint(build.build_options)'
 python -c 'from caffe2.python import core, workspace; print("GPUs found: " + str(workspace.NumCudaDevices()))'
 python -c "import onnx"
 
 echo "Congrats, you are ready to rock!!"
-echo -e "BTW, don't forget to source the environment variable script by calling ${CYAN}\"source $onnx_init_file\"${NC}"
+echo "################ Please run the following command before development ################"
+echo -e "${CYAN}source $onnx_init_file${NC}"
+echo "#####################################################################################"
