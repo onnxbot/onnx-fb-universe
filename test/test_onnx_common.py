5dd064d07da1ae68d17399059687fbf6af80aa3f
Lu Fang <lufang@fb.com>
2018-03-28 17:17:22 -0700

    Update the convert and filter scripts

--- test/test_onnx_common.py
+++ test/test_onnx_common.py
@@ -8,7 +8,7 @@ import os
 
 onnx_model_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), os.pardir, "repos", "onnx", "onnx",
-    "backend", "test", "data", "model")
+    "backend", "test", "data")
 
 
 pytorch_converted_dir = os.path.join(onnx_model_dir, "pytorch-converted")
